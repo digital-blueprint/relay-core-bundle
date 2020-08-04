ecec19b0c1fe7845bb3c31c2727242b6b10dd1ff
Christoph Reiter <reiter.christoph@gmail.com>
2020-08-04 14:51:01 +0200

    Move swagger decorator/normalizer to core

---
+++ src/Swagger/SwaggerDecorator.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace DBP\API\CoreBundle\Swagger;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+/**
+ * Class SwaggerDecorator
+ * see: https://api-platform.com/docs/core/swagger/#overriding-the-openapi-specification
+ *
+ * @package App\Swagger
+ */
+final class SwaggerDecorator implements NormalizerInterface
+{
+    private $decorated;
+    private $container;
+
+    public function __construct(NormalizerInterface $decorated, ContainerInterface $container)
+    {
+        $this->decorated = $decorated;
+        $this->container = $container;
+
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $docs = $this->decorated->normalize($object, $format, $context);
+
+        $pathsToHide = [];
+        if ($this->container->hasParameter('dbp_api.paths_to_hide')) {
+            $pathsToHide = array_merge($pathsToHide, $this->container->getParameter('dbp_api.paths_to_hide'));
+        }
+
+        foreach ($pathsToHide as $path) {
+            unset($docs["paths"][$path]);
+        }
+
+        return $docs;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return $this->decorated->supportsNormalization($data, $format);
+    }
+}
