8594ab6ed350daf55db968e5e2733913fe51dfa4
Patrizio Bekerle <patrizio.bekerle@tugraz.at>
2021-08-11 12:39:56 +0200

    Add errorid and errordetails to APIError

--- src/Exception/ApiError.php
+++ src/Exception/ApiError.php
@@ -13,4 +13,51 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
  */
 class ApiError extends HttpException
 {
+    /**
+     * @var ?string
+     */
+    private $errorId;
+
+    /**
+     * @var ?array
+     */
+    private $errorDetails;
+
+    public function __construct(int $statusCode, ?string $message = '', \Throwable $previous = null, array $headers = [], ?int $code = 0)
+    {
+        $message = json_decode($message, true);
+
+        if ($message === null)
+        {
+            $message = [
+                'message' => $message,
+                'errorId' => '',
+                'errorDetails' => null,
+            ];
+        }
+
+        parent::__construct($statusCode, json_encode($message), $previous, $headers, $code);
+    }
+
+    public function setErrorDetails(string $errorId, array $errorDetails = [])
+    {
+        $this->errorId = $errorId;
+        $this->errorDetails = $errorDetails;
+    }
+
+    public function getErrorDetails(): array
+    {
+        return [$this->errorId, $this->errorDetails];
+    }
+
+    public static function withDetails(int $statusCode, ?string $message = '', string $errorId = '', array $errorDetails = [])
+    {
+        $message = [
+            'message' => $message,
+            'errorId' => $errorId,
+            'errorDetails' => $errorDetails,
+        ];
+
+        return new ApiError($statusCode, json_encode($message));
+    }
 }
--- src/Serializer/ApiErrorNormalizer.php
+++ src/Serializer/ApiErrorNormalizer.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Dbp\Relay\CoreBundle\Serializer;
 
 use Dbp\Relay\CoreBundle\Exception\ApiError;
+use Symfony\Bundle\FrameworkBundle\Command\EventDispatcherDebugCommand;
 use Symfony\Component\ErrorHandler\Exception\FlattenException;
 use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
@@ -24,23 +25,41 @@ class ApiErrorNormalizer implements ContextAwareNormalizerInterface, NormalizerA
 
     private const ALREADY_CALLED = 'ALREADY_CALLED_'.ApiErrorNormalizer::class;
 
-    private const SUPPORTED_CLASS = APIError::class;
-
     public function normalize($object, $format = null, array $context = [])
     {
         $context[self::ALREADY_CALLED] = true;
 
         $normalized = $this->normalizer->normalize($object, $format, $context);
 
-        if ($object->getClass() === self::SUPPORTED_CLASS) {
+        if ($object->getClass() === APIError::class)
+        {
             $message = $object->getMessage();
+            $message = json_decode($message, true);
+            $errorId = $message["errorId"];
+            $errorDetails = $message["errorDetails"];
+            $message = $message["message"];
+
             if ($format === 'jsonld') {
                 if ($message !== '') {
                     $normalized['hydra:description'] = $message;
                 }
-            } elseif ($format === 'jsonproblem') {
+                if ($errorId !== '') {
+                    $normalized['dbp:errorid'] = $errorId;
+                }
+                if ($errorDetails !== null) {
+                    $normalized['dbp:errordetails'] = $errorDetails;
+                }
+            }
+            elseif ($format === 'jsonproblem')
+            {
                 if ($message !== '') {
                     $normalized['detail'] = $message;
+                }
+                if ($errorId !== '') {
+                    $normalized['errorid'] = $errorId;
+                }
+                if ($errorDetails !== null) {
+                    $normalized['errordetails'] = $errorDetails;
                 }
             }
         }
