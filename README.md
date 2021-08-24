2f5ecac1f2cd0af5acf9b8f10f1653e616403980
Christoph Reiter <reiter.christoph@gmail.com>
2021-08-24 16:33:39 +0200

    Document the bundle config and add it to the README

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-# DBP API-Core-Bundle
+# DBP Relay Core Bundle
 
-[GitLab](https://packagist.org/packages/dbp/api-core-bundle) | [Packagist](https://packagist.org/packages/dbp/api-core-bundle)
+[GitLab](https://gitlab.tugraz.at/dbp/relay/dbp-relay-core-bundle) | [Packagist](https://packagist.org/packages/dbp/relay-core-bundle)
 
-This Symfony bundle contains components required by all DBP bundles.
+## Bundle Config
+
+Created via `./bin/console config:dump-reference DbpRelayCoreBundle | sed '/^$/d'`
+
+```yaml
+# Default configuration for "DbpRelayCoreBundle"
+dbp_relay_core:
+    # Some string identifying the current build (commit hash)
+    build_info:           ~ # Example: deadbeef
+    # Some URL identifying the current build (URL to the commit on some git web interface)
+    build_info_url:       ~ # Example: 'https://gitlab.example.com/project/-/commit/deadbeef'
+    # The title text of the API docs page
+    docs_title:           'Relay API Gateway'
+    # The description text of the API docs page (supports markdown)
+    docs_description:     '*part of the [Digital Blueprint](https://gitlab.tugraz.at/dbp) project*'
+```
--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -11,17 +11,25 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('dbp_core');
+        $treeBuilder = new TreeBuilder('dbp_relay_core');
         $treeBuilder->getRootNode()
             ->children()
-            ->scalarNode('build_info')->end()
-            ->scalarNode('build_info_url')->end()
-            ->scalarNode('docs_title')
-                ->defaultValue('Relay API Gateway')
-            ->end()
-            ->scalarNode('docs_description') // This supports markdown
-                ->defaultValue('*part of the [Digital Blueprint](https://gitlab.tugraz.at/dbp) project*')
-            ->end()
+                ->scalarNode('build_info')
+                    ->info('Some string identifying the current build (commit hash)')
+                    ->example('deadbeef')
+                ->end()
+                ->scalarNode('build_info_url')
+                    ->info('Some URL identifying the current build (URL to the commit on some git web interface)')
+                    ->example('https://gitlab.example.com/project/-/commit/deadbeef')
+                ->end()
+                ->scalarNode('docs_title')
+                    ->info('The title text of the API docs page')
+                    ->defaultValue('Relay API Gateway')
+                ->end()
+                ->scalarNode('docs_description')
+                ->info('The description text of the API docs page (supports markdown)')
+                    ->defaultValue('*part of the [Digital Blueprint](https://gitlab.tugraz.at/dbp) project*')
+                ->end()
             ->end();
 
         return $treeBuilder;
