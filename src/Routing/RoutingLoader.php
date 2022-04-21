4ede1874cf2f75680fdf0aa839ee358f4c3a4b5d
Christoph Reiter <reiter.christoph@gmail.com>
2022-04-21 15:52:29 +0200

    Add a custom routing loader for bundles
    This allows bundles to add something to the "dbp_api.route_resources"
    parameter which will then be loaded as routes.
    
    For example, using a routes.yml in the bundle, add the following to the array
    in the bundle setup:
    
    [__DIR__.'/../Resources/config/routes.yaml', 'yaml']

---
+++ src/Routing/RoutingLoader.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dbp\Relay\CoreBundle\Routing;
+
+use Symfony\Component\Config\Loader\Loader;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Routing\RouteCollection;
+
+class RoutingLoader extends Loader
+{
+    /**
+     * @var ParameterBagInterface
+     */
+    private $params;
+
+    public function __construct(string $env = null, ParameterBagInterface $params)
+    {
+        parent::__construct($env);
+
+        $this->params = $params;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function load($resource, string $type = null)
+    {
+        $routes = new RouteCollection();
+
+        $routeResources = [];
+        if ($this->params->has('dbp_api.route_resources')) {
+            $routeResources = $this->params->get('dbp_api.route_resources');
+            assert(is_array($routeResources));
+        }
+
+        foreach ($routeResources as [$resource, $type]) {
+            $importedRoutes = $this->import($resource, $type);
+            $routes->addCollection($importedRoutes);
+        }
+
+        return $routes;
+    }
+
+    public function supports($resource, string $type = null): bool
+    {
+        return 'dbp_relay' === $type;
+    }
+}
