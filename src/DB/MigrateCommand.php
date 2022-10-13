6023ec50585fd85452a1788ad4b99b2898e01b97
Christoph Reiter <reiter.christoph@gmail.com>
2022-10-13 12:21:36 +0200

    migrate: spawn separate processes for each migration
    Turns out the migration bundle sets some global state which it
    doesn't update on the next migration run, resulting in the wrong
    entity manager being selected for some migrations.
    
    Since it doesn't seem to be designed for calling it multiple times
    in the same process try to isolate the calls by spawning a new
    PHP process each time.
    
    This assumes we find the right PHP executable and console command
    on the filesystem, but hopefully this works in the common case.
    If not we can try to use argv[0] for example.

--- src/DB/MigrateCommand.php
+++ src/DB/MigrateCommand.php
@@ -8,9 +8,12 @@ use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\NullLogger;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Process\Process;
 
 /**
  * A command for migrating the DBs and setting up other things like the worker
@@ -22,11 +25,13 @@ final class MigrateCommand extends Command implements LoggerAwareInterface
 
     protected static $defaultName = 'dbp:relay:core:migrate';
     private $entityManagers;
+    private $appKernel;
 
-    public function __construct()
+    public function __construct(KernelInterface $appKernel)
     {
         parent::__construct();
         $this->logger = new NullLogger();
+        $this->appKernel = $appKernel;
     }
 
     protected function configure()
@@ -39,6 +44,54 @@ final class MigrateCommand extends Command implements LoggerAwareInterface
         $this->entityManagers = $entityManagers;
     }
 
+    /**
+     * Runs a console command in a separate process using the same PHP executable while writing output to $output.
+     * This is required because some commands like doctrine:migrations:migrate break when called
+     * with different settings in the same process (the connection doesn't despite a different --em being passed).
+     * This isolates the different calls.
+     *
+     * The goal is to be a close as possible to the behaviour of invoking a symfony command in-process, but
+     * with isolation between multiple commands.
+     *
+     * This will throw in case the command doesn't return 0;
+     *
+     * This assumes <project-dir>/bin/console exists (which it hopefully does)
+     */
+    public function runConsoleCommand(array $inputArgs, InputInterface $input, OutputInterface $output)
+    {
+        // Ensure everything is where we expect it to be
+        $filesystem = new Filesystem();
+        $console = $this->appKernel->getProjectDir().DIRECTORY_SEPARATOR.'bin'.DIRECTORY_SEPARATOR.'console';
+        if (!$filesystem->exists($console)) {
+            throw new \RuntimeException("$console doesn't exist");
+        }
+        if (!$filesystem->exists(PHP_BINARY)) {
+            throw new \RuntimeException(PHP_BINARY." doesn't exist");
+        }
+
+        // Build a console command
+        $args = [PHP_BINARY, $console];
+        $isTty = true;
+        if (!$input->isInteractive()) {
+            $args[] = '--no-interaction';
+            $isTty = false;
+        }
+        $args = array_merge($args, $inputArgs);
+
+        // Run the command
+        $output->writeln('Running command: '.implode(' ', $args));
+        $process = new Process($args);
+        $process->setTty($isTty);
+        $process->mustRun(function ($type, $buffer) use ($output) {
+            $errOutput = $output instanceof ConsoleOutputInterface ? $output->getErrorOutput() : $output;
+            if ($type === Process::ERR) {
+                $errOutput->write($buffer);
+            } else {
+                $output->write($buffer);
+            }
+        });
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $app = $this->getApplication();
@@ -48,21 +101,14 @@ final class MigrateCommand extends Command implements LoggerAwareInterface
         // In theory symfony should do this automatically, but it is broken :(
         // https://github.com/symfony/symfony/issues/47005
         $output->writeln('[Set up the messenger transports]');
-        $command = $app->find('messenger:setup-transports');
-        $runInput = new ArrayInput([]);
-        $runInput->setInteractive($input->isInteractive());
         // Note: in case the transport doesn't have/support auto setup then it will
         // just print a notice, so we don't have to handle it here atm.
-        $status = $command->run($input, $output);
-        if ($status !== Command::SUCCESS) {
-            return $status;
-        }
+        $this->runConsoleCommand(['messenger:setup-transports'], $input, $output);
 
         // The locking system might also create a DB table, but it doesn't expose an easy way
         // to handle it backend independent, so this isn't handled here.
 
         // Then migrate all registered DBs
-        $command = $app->find('doctrine:migrations:migrate');
         if (count($this->entityManagers) === 0) {
             $output->writeln('No entity managers registered, nothing to migrate');
         } else {
@@ -73,14 +119,9 @@ final class MigrateCommand extends Command implements LoggerAwareInterface
         }
         foreach ($this->entityManagers as $em) {
             $output->writeln("Migrating $em:");
-            $runInput = new ArrayInput(['--em' => $em]);
-            $runInput->setInteractive($input->isInteractive());
-            $status = $command->run($runInput, $output);
-            if ($status !== Command::SUCCESS) {
-                return $status;
-            }
+            $this->runConsoleCommand(['doctrine:migrations:migrate', '--em',  $em], $input, $output);
         }
 
-        return $status;
+        return Command::SUCCESS;
     }
 }
