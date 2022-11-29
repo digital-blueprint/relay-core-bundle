ce8d85011f6990e4c3fcfeff8700d6f984e5df9a
Christoph Reiter <reiter.christoph@gmail.com>
2022-11-29 15:53:19 +0100

    AuthorizationDataProviderInterface: make the identifier optional
    This way we can return attributes even for system users, assuming they
    are not tied to some user.
    
    This is an API break, so bundles need to be updated.

--- src/Authorization/AuthorizationDataProviderInterface.php
+++ src/Authorization/AuthorizationDataProviderInterface.php
@@ -7,9 +7,20 @@ namespace Dbp\Relay\CoreBundle\Authorization;
 interface AuthorizationDataProviderInterface
 {
     /**
+     * Returns a list of available attributes.
+     *
      * @return string[]
      */
     public function getAvailableAttributes(): array;
 
-    public function getUserAttributes(string $userIdentifier): array;
+    /**
+     * Returns an array of attribute names and values.
+     * In case an attribute has no value then it isn't contained.
+     *
+     * In case $userIdentifier is null then there is no active user and
+     * only globally available attributes shold be returned.
+     *
+     * @return array<string, mixed>
+     */
+    public function getUserAttributes(?string $userIdentifier): array;
 }
