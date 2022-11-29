13eb1f369f9901b54e3829886d63f7b21873f7c7
Christoph Reiter <reiter.christoph@gmail.com>
2022-11-29 15:15:35 +0100

    Remove unused const

--- src/Authorization/AuthorizationException.php
+++ src/Authorization/AuthorizationException.php
@@ -6,7 +6,6 @@ namespace Dbp\Relay\CoreBundle\Authorization;
 
 class AuthorizationException extends \RuntimeException
 {
-    public const ROLE_UNDEFINED = 1;
     public const PRIVILEGE_UNDEFINED = 2;
     public const ATTRIBUTE_UNDEFINED = 3;
     public const INFINITE_LOOP_DETECTED = 4;
