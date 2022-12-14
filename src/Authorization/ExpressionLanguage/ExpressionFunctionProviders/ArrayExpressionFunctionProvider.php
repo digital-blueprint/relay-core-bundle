7205d525fd3193d7e6cca81f3ac7dcc71a3fc880
Tobias Gross-Vogt <tgros@tugraz.at>
2022-12-14 14:33:37 +0100

    added function provider to expression language

---
+++ src/Authorization/ExpressionLanguage/ExpressionFunctionProviders/ArrayExpressionFunctionProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dbp\Relay\CoreBundle\Authorization\ExpressionLanguage\ExpressionFunctionProviders;
+
+use Symfony\Component\ExpressionLanguage\ExpressionFunction;
+use Symfony\Component\ExpressionLanguage\ExpressionFunctionProviderInterface;
+
+class ArrayExpressionFunctionProvider implements ExpressionFunctionProviderInterface
+{
+    public function getFunctions(): array
+    {
+        return [
+            new ExpressionFunction('empty',
+                function (string $varName): string {
+                    return sprintf('empty(%s)', $varName);
+                },
+                function ($arguments, $varName): bool {
+                    return empty($varName);
+                }),
+        ];
+    }
+}
