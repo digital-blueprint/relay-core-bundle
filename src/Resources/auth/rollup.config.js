6a2ed05e0208df75ddda70d5e940589d4b693e15
Christoph Reiter <reiter.christoph@gmail.com>
2023-04-26 10:54:45 +0200

    Drop unused rollup.utils.js

--- src/Resources/auth/rollup.config.js
+++ src/Resources/auth/rollup.config.js
@@ -6,7 +6,6 @@ import {terser} from "rollup-plugin-terser";
 import json from '@rollup/plugin-json';
 import serve from 'rollup-plugin-serve';
 import del from 'rollup-plugin-delete';
-import {getPackagePath, getDistPath} from './rollup.utils.js';
 
 const pkg = require('./package.json');
 const build = (typeof process.env.BUILD !== 'undefined') ? process.env.BUILD : 'local';
