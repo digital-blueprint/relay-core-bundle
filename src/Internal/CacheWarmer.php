2928e20585cadad7c5e2384a01fb9901f1cda5c7
Christoph Reiter <reiter.christoph@gmail.com>
2023-05-23 14:09:36 +0200

    Add a dummy cache warmer which uses the application cache to setup the cache
    In case the application cache uses a pdo adapter and there are no existing cache
    saves it would spam the log with SQL errors due to cache misses, because it has
    no logic to automatically create the database and table on a cache miss.
    
    Work around this by adding a custom cache warmer which saves something to the
    cache and deletes it again.

---
+++ src/Internal/CacheWarmer.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dbp\Relay\CoreBundle\Internal;
+
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
+use Symfony\Component\Uid\Uuid;
+
+class CacheWarmer implements CacheWarmerInterface
+{
+    /**
+     * @var AdapterInterface
+     */
+    private $adapter;
+
+    public function __construct(AdapterInterface $adapter)
+    {
+        $this->adapter = $adapter;
+    }
+
+    public function isOptional(): bool
+    {
+        return true;
+    }
+
+    public function warmUp(string $cacheDir): array
+    {
+        // This is a workaround for https://github.com/symfony/symfony/issues/32569
+        // Create a dummy cache entry, so that the cache database and table get created,
+        // so we don't get spammed with errors on cache misses
+        $key = (Uuid::v4())->toRfc4122();
+        $item = $this->adapter->getItem($key);
+        $item->set(true);
+        $this->adapter->save($item);
+        $this->adapter->deleteItem($key);
+
+        return [];
+    }
+}
