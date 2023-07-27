8a0ab1e88dfdd50b20a8be9e8e37dd2280566fcd
Christoph Reiter <reiter.christoph@gmail.com>
2023-07-27 13:50:43 +0200

    StateProviderTrait: also handle delete/put/patch operations
    They also need state providers to fetch the current state of object
    that is about to be modified/deleted.
    
    Differentiate via CollectionOperationInterface as suggested in the
    api-platform docs.

--- src/ApiPlatform/State/StateProviderTrait.php
+++ src/ApiPlatform/State/StateProviderTrait.php
@@ -4,28 +4,21 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\CoreBundle\ApiPlatform\State;
 
-use ApiPlatform\Metadata\Get;
-use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\CollectionOperationInterface;
 use ApiPlatform\Metadata\Operation;
-use Dbp\Relay\CoreBundle\Exception\ApiError;
 use Dbp\Relay\CoreBundle\Rest\Query\Pagination\PartialPaginator;
-use Symfony\Component\HttpFoundation\Response;
 
 trait StateProviderTrait
 {
     /**
      * @return PartialPaginator|object|null
-     *
-     * @throws \Exception
      */
     public function provide(Operation $operation, array $uriVariables = [], array $context = [])
     {
-        if ($operation instanceof GetCollection) {
+        if ($operation instanceof CollectionOperationInterface) {
             return $this->getCollectionInternal($context);
-        } elseif ($operation instanceof Get) {
+        } else {
             return $this->getItemInternal($uriVariables['identifier'], $context);
         }
-
-        throw ApiError::withDetails(Response::HTTP_INTERNAL_SERVER_ERROR, 'unknown provider operation: '.$operation->getShortName());
     }
 }
