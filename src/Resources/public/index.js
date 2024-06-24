6c359ed6de8f00b7958c1461119b3d4ba84dbf25
Christoph Reiter <reiter.christoph@gmail.com>
2024-06-24 11:51:59 +0200

    Fix the API docs auto-login with api-platform v3.3.6
    api-platform v3.3.6 updated the swagger UI version which changed some things,
    which broke our hack to inject the API token automatically.
    
    Since we added this hack api-platform has started to expose the swaggerui object
    under "window.swaggerUI", so ww can now just use "preauthorizeApiKey()" to set the
    token directly. Passing "null" there removes the api key again.
    
    Also due to some template changes the CSS that was supposed to hide the login
    button no longer worked, so adjust that too, to be less specific.
    
    In theory this should work with all supported api-platform versions again.

--- src/Resources/public/index.js
+++ src/Resources/public/index.js
@@ -1,39 +1,5 @@
 import './auth/api-platform-auth.js';
 
-function log(message) {
-    console.log('API docs: ' + message);
-}
-
-function useToken(token) {
-    var authContainer = null;
-    var btn = document.getElementsByClassName('btn authorize unlocked')[0];
-    if (btn !== undefined) {
-        btn.click();
-        authContainer = document.getElementsByClassName('auth-container')[0];
-    } else {
-        btn = document.getElementsByClassName('btn authorize locked')[0];
-        btn.click();
-        authContainer = document.getElementsByClassName('auth-container')[0];
-        authContainer.children[0].children[1].children[0].click(); // Logout
-    }
-
-    var pwInput = authContainer.children[0].children[0].children[0].children[4].children[1].children[0];
-    var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
-    if (token)
-        token = 'Bearer ' + token;
-    nativeInputValueSetter.call(pwInput, token); //'react 16 value');
-
-    var ev2 = new Event('input', { bubbles: true});
-    pwInput.dispatchEvent(ev2);
-
-    // --------
-    var btnLogin = authContainer.children[0].children[1].children[0];
-    btnLogin.click(); // Login
-    authContainer.children[0].children[1].children[1].click(); // Close
-    if (token)
-        log('New token set');
-}
-
 var delayInsertTimer = 0;
 
 function getKeycloakServerUrl() {
@@ -104,7 +70,6 @@ function insertDBPContainer() {
     var section = target.children[0];
     section.insertBefore(element, section.children[0]);
     window.clearInterval(delayInsertTimer);
-    log('insertDBPContainer done');
 }
 
 function delayInsert() {
@@ -114,7 +79,7 @@ function delayInsert() {
 document.addEventListener('DOMContentLoaded', delayInsert);
 
 function onAuthUpdate(e) {
-    useToken(e.detail.token);
+    window.swaggerUI.preauthorizeApiKey('apiKey', e.detail.token);
 }
 
 window.addEventListener("api-platform-auth-update", onAuthUpdate);
