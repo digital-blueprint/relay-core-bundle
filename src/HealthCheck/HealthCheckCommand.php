9018dc5e1280879524338d06ef4dadd5d476f9f9
Christoph Reiter <reiter.christoph@gmail.com>
2025-06-17 11:04:58 +0200

    health-checks: add option to list checks, and to filter them
    New "--only" option which takes a comma separated list of
    check names to run.
    
    New "--list-checks" option to list all available checks.

--- src/HealthCheck/HealthCheckCommand.php
+++ src/HealthCheck/HealthCheckCommand.php
@@ -8,7 +8,9 @@ use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class HealthCheckCommand extends Command implements LoggerAwareInterface
 {
@@ -34,6 +36,19 @@ class HealthCheckCommand extends Command implements LoggerAwareInterface
     {
         $this->setName('dbp:relay:core:check-health');
         $this->setDescription('Run all available health checks');
+        $this
+            ->addOption(
+                'only',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Run only specific health checks (comma-separated list)'
+            );
+        $this->addOption(
+            'list-checks',
+            null,
+            InputOption::VALUE_NONE,
+            'List all available health checks'
+        );
     }
 
     /**
@@ -44,10 +59,82 @@ class HealthCheckCommand extends Command implements LoggerAwareInterface
         $this->checks = $checks;
     }
 
+    private function listAvailableChecks(SymfonyStyle $io): void
+    {
+        $io->title('Available Health Checks');
+
+        $rows = [];
+        foreach ($this->checks as $check) {
+            $rows[] = [$check->getName()];
+        }
+        asort($rows);
+
+        $io->table(['Check Name'], $rows);
+    }
+
+    private function determineChecksToRun(InputInterface $input, SymfonyStyle $io): ?array
+    {
+        $onlyOption = $input->getOption('only');
+
+        if ($onlyOption === null) {
+            return $this->checks;
+        }
+
+        $requestedCheckNames = array_map('trim', explode(',', $onlyOption));
+        $requestedCheckNames = array_filter($requestedCheckNames);
+
+        if ($requestedCheckNames === []) {
+            $io->error('No health checks specified in --only option.');
+
+            return null;
+        }
+
+        $availableChecks = [];
+        foreach ($this->checks as $check) {
+            $availableChecks[$check->getName()] = $check;
+        }
+
+        $checksToRun = [];
+        $invalidChecks = [];
+
+        foreach ($requestedCheckNames as $checkName) {
+            if (isset($availableChecks[$checkName])) {
+                $checksToRun[] = $availableChecks[$checkName];
+            } else {
+                $invalidChecks[] = $checkName;
+            }
+        }
+
+        if (!empty($invalidChecks)) {
+            $io->error(sprintf(
+                'Invalid health check(s): %s. Available checks: %s',
+                implode(', ', $invalidChecks),
+                implode(', ', array_keys($availableChecks))
+            ));
+
+            return null;
+        }
+
+        return $checksToRun;
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $io = new SymfonyStyle($input, $output);
+
+        if ($input->getOption('list-checks')) {
+            $this->listAvailableChecks($io);
+
+            return Command::SUCCESS;
+        }
+
+        $checksToRun = $this->determineChecksToRun($input, $io);
+        if ($checksToRun === null) {
+            return Command::FAILURE;
+        }
+
         $failed = [];
-        foreach ($this->checks as $check) {
+        foreach ($checksToRun as $check) {
             $output->writeln('<fg=green;options=bold>['.$check->getName().']</>');
             $results = $check->check(new CheckOptions());
             foreach ($results as $result) {
