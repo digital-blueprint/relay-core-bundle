5b575ac05ceacafcee93bed4fb0f30d3b1e31b80
Christoph Reiter <reiter.christoph@gmail.com>
2025-11-03 15:19:50 +0100

    auth: drop rollup support

--- src/Resources/auth/rollup.config.js
+++ src/Resources/auth/rollup.config.js
@@ -1,17 +1,11 @@
-import {globSync} from 'glob';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import {globSync} from 'node:fs';
 import copy from 'rollup-plugin-copy';
-import terser from "@rollup/plugin-terser";
-import json from '@rollup/plugin-json';
 import serve from 'rollup-plugin-serve';
-import del from 'rollup-plugin-delete';
 import process from 'node:process';
 
 const build = (typeof process.env.BUILD !== 'undefined') ? process.env.BUILD : 'local';
 console.log("build: " + build);
-const useTerser = (process.env.ROLLUP_WATCH !== 'true' && build !== 'test');
-let isRolldown = process.argv.some((arg) => arg.includes('rolldown'));
+const prodBuild = (process.env.ROLLUP_WATCH !== 'true' && build !== 'test');
 
 export default (async () => {
     return {
@@ -21,18 +15,13 @@ export default (async () => {
             entryFileNames: '[name].js',
             chunkFileNames: 'shared/[name].[hash].js',
             format: 'esm',
-            sourcemap: true
+            sourcemap: true,
+            minify: prodBuild,
+            cleanDir: true,
         },
         plugins: [
-            del({
-                targets: '../public/auth/*',
-                force: true
-            }),
-            !isRolldown && resolve({browser: true}),
-            !isRolldown && commonjs(),
-            !isRolldown && json(),
-            useTerser ? terser() : false,
             copy({
+                hook: 'generateBundle',
                 targets: [
                     {src: 'assets/index.html', dest: '../public/auth'},
                     {src: 'assets/silent-check-sso.html', dest: '../public/auth'},
