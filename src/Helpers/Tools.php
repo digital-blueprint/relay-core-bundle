991c46d2966ed36a9718ead8f5c456de0254abf2
Christoph Reiter <reiter.christoph@gmail.com>
2025-12-09 09:30:01 +0100

    Remove some deprecated helper functions

--- src/Helpers/Tools.php
+++ src/Helpers/Tools.php
@@ -6,37 +6,10 @@ namespace Dbp\Relay\CoreBundle\Helpers;
 
 class Tools
 {
-    /**
-     * Like json_decode but throws on invalid json data.
-     *
-     * @deprecated use json_decode() with JSON_THROW_ON_ERROR instead
-     *
-     * @return mixed
-     *
-     * @throws \JsonException
-     */
-    public static function decodeJSON(string $json, bool $assoc = false)
-    {
-        $result = json_decode($json, $assoc);
-        $json_error = json_last_error();
-        if ($json_error !== JSON_ERROR_NONE) {
-            throw new \JsonException(sprintf('%s: "%s"', json_last_error_msg(), print_r($json, true)));
-        }
-
-        return $result;
-    }
-
     public static function filterErrorMessage(string $message): string
     {
         // hide token parameters
         return preg_replace('/([&?]token=)[\w\d-]+/i', '${1}hidden', $message);
-    }
-
-    /**
-     * @deprecated
-     */
-    public static function dumpTrace(...$moreVars)
-    {
     }
 
     public static function isNullOrEmpty(?string $str): bool
