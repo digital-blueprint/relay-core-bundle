6f58dce7b388d01637af77be08d507ecf8cd324e
Christoph Reiter <reiter.christoph@gmail.com>
2026-09-17 11:50:51 +0200

    auth: enable type aware linting

--- src/Resources/auth/rolldown.config.js
+++ src/Resources/auth/rolldown.config.js
@@ -7,30 +7,28 @@ const build = (typeof process.env.BUILD !== 'undefined') ? process.env.BUILD : '
 console.log("build: " + build);
 const prodBuild = (process.env.ROLLUP_WATCH !== 'true' && build !== 'test');
 
-export default (async () => {
-    return {
-        transform: {
-            target: prodBuild ? ['chrome106', 'firefox110', 'safari16'] : 'esnext',
-        },
-        input: (build != 'test') ? ['src/api-platform-auth.js'] : globSync('test/**/*.js'),
-        output: {
-            dir: (build != 'test') ? '../public/auth' : 'dist',
-            entryFileNames: '[name].js',
-            chunkFileNames: 'shared/[name].[hash].js',
-            format: 'esm',
-            sourcemap: true,
-            minify: prodBuild,
-            cleanDir: true,
-        },
-        plugins: [
-            copy({
-                hook: 'generateBundle',
-                targets: [
-                    {src: 'assets/index.html', dest: '../public/auth'},
-                    {src: 'assets/silent-check-sso.html', dest: '../public/auth'},
-                ],
-            }),
-            (process.env.ROLLUP_WATCH === 'true') ? serve({contentBase: '../public/auth', host: '127.0.0.1', port: 8002}) : false
-        ]
-    };
-})();
+export default Promise.resolve({
+    transform: {
+        target: prodBuild ? ['chrome106', 'firefox110', 'safari16'] : 'esnext',
+    },
+    input: (build != 'test') ? ['src/api-platform-auth.js'] : globSync('test/**/*.js'),
+    output: {
+        dir: (build != 'test') ? '../public/auth' : 'dist',
+        entryFileNames: '[name].js',
+        chunkFileNames: 'shared/[name].[hash].js',
+        format: 'esm',
+        sourcemap: true,
+        minify: prodBuild,
+        cleanDir: true,
+    },
+    plugins: [
+        copy({
+            hook: 'generateBundle',
+            targets: [
+                {src: 'assets/index.html', dest: '../public/auth'},
+                {src: 'assets/silent-check-sso.html', dest: '../public/auth'},
+            ],
+        }),
+        (process.env.ROLLUP_WATCH === 'true') ? serve({contentBase: '../public/auth', host: '127.0.0.1', port: 8002}) : false
+    ]
+});
